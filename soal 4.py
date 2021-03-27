a86f2b6352941c3c94a54a7891376c5cd686983c
dendyhalim8 <79240184+dendyhalim8@users.noreply.github.com>
2021-03-27 10:40:22 +0700

    Update soal 4.py

--- soal 4.py
+++ soal 4.py
@@ -1,14 +1,20 @@
 usia = int(input("Berapa usia kamu?"))
 ujian = input("Apakah Anda sudah lulus ujian kualifikasi (Y / T)?")
-Y = 1
-T = 0
 
 if usia >= 21:
     usia = 1
 else:
     usia = 0
 
-hasil = usia+Y+T
+if ujian == "Y":
+    ujian = 1
+elif ujian == "T":
+    ujian = 0
+else:
+    print("Jawaban yang anda masukan salah")
+    ujian = 0
+
+hasil = usia+ujian
 if hasil == 2:
     hasil = "Anda dapat mendaftar di kursus."
 else:
